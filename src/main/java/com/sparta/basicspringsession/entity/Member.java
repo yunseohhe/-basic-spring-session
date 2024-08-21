344503676e5c30f622fede08398ffee5127aa698
yunseohhe <dbstj4566@naver.com>
2024-08-21 17:29:15 +0900

    기본 구조 세팅

---
+++ src/main/java/com/sparta/basicspringsession/entity/Member.java
@@ -0,0 +1,18 @@
+package com.sparta.basicspringsession.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Entity
+@NoArgsConstructor
+public class Member {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+}
