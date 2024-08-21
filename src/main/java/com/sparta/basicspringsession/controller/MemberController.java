58f7aee5dece1e639c402016a0b5746a9abba619
yunseohhe <dbstj4566@naver.com>
2024-08-21 19:44:59 +0900

    특정 Id 조회하는 부분 구현 완료

--- src/main/java/com/sparta/basicspringsession/controller/MemberController.java
+++ src/main/java/com/sparta/basicspringsession/controller/MemberController.java
@@ -1,14 +1,12 @@
 package com.sparta.basicspringsession.controller;
 
+import com.sparta.basicspringsession.dto.MemberDetailResponseDto;
 import com.sparta.basicspringsession.dto.MemberSaveRequestDto;
 import com.sparta.basicspringsession.dto.MemberSaveResponseDto;
 import com.sparta.basicspringsession.dto.MemberSimpleResponseDto;
 import com.sparta.basicspringsession.service.MemberService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -18,14 +16,20 @@ public class MemberController {
 
     private final MemberService memberService;
 
-    @PostMapping("/members")
+    @PostMapping("/members") // 추가
     public MemberSaveResponseDto saveMember(@RequestBody MemberSaveRequestDto memberSaveRequestDto) {
         return memberService.saveMember(memberSaveRequestDto);
     }
 
-    @GetMapping("/members")
+    @GetMapping("/members") // 추가한 모든 것들을 조회
     public List<MemberSimpleResponseDto> getMembers() {
         return memberService.getMembers();
     }
+
+    @GetMapping("/members/{memberId}") // 특정 Id 조회
+    public MemberDetailResponseDto getMember(@PathVariable Long memberId) {
+        return memberService.getMember(memberId);
+    }
+
 
 }
--- src/main/java/com/sparta/basicspringsession/service/MemberService.java
+++ src/main/java/com/sparta/basicspringsession/service/MemberService.java
@@ -1,5 +1,6 @@
 package com.sparta.basicspringsession.service;
 
+import com.sparta.basicspringsession.dto.MemberDetailResponseDto;
 import com.sparta.basicspringsession.dto.MemberSaveRequestDto;
 import com.sparta.basicspringsession.dto.MemberSaveResponseDto;
 import com.sparta.basicspringsession.dto.MemberSimpleResponseDto;
@@ -8,6 +9,7 @@ import com.sparta.basicspringsession.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -34,6 +36,11 @@ public class MemberService {
             memberSimpleResponseDtos.add(new MemberSimpleResponseDto(member.getId(), member.getName()));
         }
         return memberSimpleResponseDtos;
+    }
+
+    public MemberDetailResponseDto getMember(Long memberId) {
+        Member member = memberRepository.findById(memberId).orElseThrow(() -> new NullPointerException("멤버가 없습니다."));
+        return new MemberDetailResponseDto(member.getId(), member.getName());
     }
 
 }
